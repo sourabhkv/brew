67b6de8555249d5a77c71fa6652739108332697b
Sourabh kumar verma <55890376+sourabhkv@users.noreply.github.com>
2023-01-03 23:33:00 +0530

    Create P10.java

---
+++ lab/P10.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Division {
+	public static void main(String[] args) {
+		int a,b,result;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Input two integers");
+		a=input.nextInt();
+		b=input.nextInt();
+		try {
+			result = a/b;
+			System.out.println("Result = "+result);
+		}
+		catch(ArithematicException e) {
+			System.out.println("exception caught Division by zero error "+e);
+		}
+	}
+}
